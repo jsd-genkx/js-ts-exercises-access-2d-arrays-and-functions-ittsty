bfda177c880fcf1f38964c2664975d8bcc7940ef
PatcharaP <itt.patchara@gmail.com>
2025-11-24 16:30:58 +0700

    add move func

--- game-fn.ts
+++ game-fn.ts
@@ -1,30 +1,61 @@
 const board = [
-	["A", "B", "C"],
-	["D", "E", "F"],
-	["G", "H", "I"],
+  ["A", "B", "C"],
+  ["D", "E", "F"],
+  ["G", "H", "I"],
 ];
 
 const moves = [];
-
+const colLength = board[0].length;
 // Start at A (0,0) -> [row][col]
+
 let row = 0;
 let col = 0;
 moves.push(board[row][col]); // A
 
 // Define movement functions
-function moveRight() {}
+function moveRight() {
+  if (col < board[row].length - 1) {
+    col++;
+    moves.push(board[row][col]);
+  } else {
+    console.log(`Out of bound at ${board[row][col]}`);
+  }
+}
 
-function moveLeft() {}
+function moveLeft() {
+  if (col > 0) {
+    col--;
+    moves.push(board[row][col]);
+  } else {
+    console.log(`Out of bound at ${board[row][col]}`);
+  }
+}
 
-function moveUp() {}
+function moveUp() {
+  if (row > 0) {
+    row--;
+    moves.push(board[row][col]);
+  } else {
+    console.log(`Out of bound at ${board[row][col]}`);
+  }
+}
 
-function moveDown() {}
+function moveDown() {
+  if (row < colLength - 1)
+  {
+    row++;
+    moves.push(board[row][col]);
+  } else {
+    console.log(`Out of bound at ${board[row][col]}`);
+  }
+}
 
 // Call the movement functions
 moveRight(); // Move from A to B
 moveRight(); // Move from B to C
 moveDown(); // Move from C to F
 moveLeft(); // Move from F to E
+
 
 console.log("Path:", moves.join(" → "));
 console.log("Total Moves (Start from 'A'):", moves.length - 1);
--- game.ts
+++ game.ts
@@ -16,11 +16,16 @@ col++;
 moves.push(board[row][col]); // B
 
 // Move Right to C
-
+col++;
+moves.push(board[row][col]);
 // Move Down to F
 
+row++;
+moves.push(board[row][col]);
 // Move left to E
 
+col--;
+moves.push(board[row][col]);
 console.log("Path:", moves.join(" → "));
 // Output: Path: A → B → C → F → E
 console.log("Total Moves (Start from 'A'):", moves.length - 1);
